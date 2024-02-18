99407bcc1caff981194c12b5fc806b91cddd8ea1
Lunareix <me@raihanpk.com>
2024-02-18 23:13:58 +0700

    Update Heading.tsx

--- components/homepage/Heading.tsx
+++ components/homepage/Heading.tsx
@@ -6,7 +6,7 @@ const Heading = () => {
   return (
     <h1 className="font-medium text-neutral-900 dark:text-neutral-200">
       Namaku <span>{siteMetadata.fullName}</span> - seorang <span>mahasiswa</span> di{' '}
-      <span className="hidden">Bogor, Indonesia</span>
+      <span className="hidden">Bogor, Indonesia.</span>
       <span className="absolute ml-1.5 inline-flex pt-[3px]">
         <Twemoji emoji="indonesia-flag" />
       </span>
